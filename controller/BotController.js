278c7ffd9498fea3ca99aeaaf6ac9b0dad84c944
tnzo <lucas.giovenardi@gmail.com>
2018-07-26 18:25:36 -0300

    Criacao de rotas e controlers
    Criada rotas e controladores.
    Configurado no index.js a rota.

---
+++ controller/BotController.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.processRequest = function(req, res) {
+    //if (req.body.result.action == "localizacaoPedido") {
+      getLocalizacaoPedido(req,res)
+    //} else if (req.body.result.action == "tell.about") {
+    //    getTeamInfo(req,res)
+    //}
+};
+
+function getLocalizacaoPedido(req,res) {
+  
+  return res.json({
+        speech: 'Seu pedido está em trânsito',
+        displayText: 'Seu pedido está em trânsito',
+        source: 'localizacaoPedido' });
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var express 	= require('express');
-var bodyParser = require('body-parser');
+var bodyParser 	= require('body-parser');
 var http		= require('http');
+var routes		= require('./routes/routes');
 var server 		= express();
 var msg;
 
@@ -15,6 +16,9 @@ server.get('/', function (req, res) {
   //res.send('Hello World!');
   res.json(msg)
 });
+
+//var routes = require('./API/Routes/Routes'); //importing route
+routes(server);
 
 server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and listening on port" + process.env.PORT);
