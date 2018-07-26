e5fa11c3e650a38907e0040e665e6c2f857ef850
tnzo <lucas.giovenardi@gmail.com>
2018-07-26 15:47:30 -0300

    Modificando retorno para da API

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict';
 
-var express = require('express');
-var server = express();
+var express 	= require('express');
+var body-parser = require('body-parser');
+var http		= require('http');
+var server 		= express();
+var msg;
+
+msg = ""
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 server.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  //res.send('Hello World!');
+  res.json({msg: "Lucas Toniazzo"})
+
 });
 
 server.listen((process.env.PORT || 8000), function () {
