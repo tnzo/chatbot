eb73d5008909c093f2c1fb6804e92f058d7fc4fc
tnzo <lucas.giovenardi@gmail.com>
2018-07-26 14:27:53 -0300

    Atualizado index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.get('/', function (req, res) {
+var express = require('express');
+var server = express();
+
+server.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen((process.env.PORT || 8000), function () {
+server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and listening on port" + process.env.PORT);
 });
-
-//app.listen(3000, function () {
-//  console.log('Example app listening on port 3000!');
-//});
